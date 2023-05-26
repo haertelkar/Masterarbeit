98e9c26353d9a63093cce9dd615929724b5a445a
Karl <haertelk@hu-berlin.de>
2023-05-26 17:37:10 +0200

    Clean Up script

---
+++ CleanUp.py
@@ -0,0 +1,19 @@
+import shutil
+import os
+from tqdm import tqdm
+
+emptyDirs = set()
+
+for dirpath, dirnames, filenames in tqdm(os.walk(".")):
+    if "measurements" not in dirpath:
+        continue
+    for filename in filenames:
+        if ".npy" in filename or ".csv" in filename:
+            os.remove(os.path.join(dirpath, filename))
+            emptyDirs.add(dirpath)
+
+
+print("Following directories were emptied:")
+
+for dir in emptyDirs:
+    print(dir)
