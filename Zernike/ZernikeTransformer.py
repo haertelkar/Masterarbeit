6b5b400dec698b358cd147bcfaf316502df6d4ca
Karl Haertel <k.haertel@yahoo.com>
2023-05-31 16:56:57 +0200

    reduced redundancy and used os.path.join

--- Zernike/ZernikeTransformer.py
+++ Zernike/ZernikeTransformer.py
@@ -15,50 +15,50 @@ class ZernikeMoments:
 		# return the Zernike moments for the image
 		return mahotas.features.zernike_moments(image, self.radius, degree = 8)
 
-imgPathTest = "..\\FullPixelGridML\\measurements_test"
+for testOrTrain in ["test", "train"]:
 
-with open('measurements_test\\labels.csv', 'w+', newline='') as labelsZernike, open(imgPathTest + '\\labels.csv', 'r', newline='') as labelsFullPixelGrid:
-    Writer = csv.writer(labelsZernike, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    Reader = csv.reader(labelsFullPixelGrid, delimiter=',', quotechar='|')
-    moments = None
-    for firstRow in Reader:
-        Writer.writerow(["fileName","element","xAtomRel","yAtomRel","zAtoms"])
-        break
+    imgPath = os.path.join("..","FullPixelGridML",f"measurements_{testOrTrain}")
 
-    for row in tqdm(Reader, desc = "Converting Test Data"):
-        if "element" in row:
-            continue
-        fileName, element, xAtomRel, xAtomShift, yAtomRel, yAtomShift, zAtoms = row
-        if ".npy" not in fileName:
-            raise Exception(fileName + " is not a valid filename")
-        image = np.load(os.path.join(imgPathTest, fileName))
-        if moments is None:
-            radius = 7#int(len(image)/2) - 1 
-            desc = ZernikeMoments(radius)
-        moments = desc.describe(image)          
-        np.save("measurements_test\\" + fileName, moments)
-        
-        #Writer.writerow([fileName] + [str(difParams) for difParams in [element, xAtomRel, xAtomShift, yAtomRel, yAtomShift]])    
-        Writer.writerow([fileName] + [str(difParams) for difParams in [element, xAtomRel, yAtomRel, zAtoms]])
+    with open(os.path.join(f'measurements_{testOrTrain}','labels.csv'), 'w+', newline='') as labelsZernike, open(os.path.join(imgPath, 'labels.csv'), 'r', newline='') as labelsFullPixelGrid:
+        Writer = csv.writer(labelsZernike, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        Reader = csv.reader(labelsFullPixelGrid, delimiter=',', quotechar='|')
+        moments = None
+        for firstRow in Reader:
+            Writer.writerow(["fileName","element","xAtomRel","yAtomRel","zAtoms"])
+            break
 
-imgPathTrain = "..\\FullPixelGridML\\measurements_train"
+        for row in tqdm(Reader, desc = f"Converting {testOrTrain} Data"):
+            if "element" in row:
+                continue
+            fileName, element, xAtomRel, xAtomShift, yAtomRel, yAtomShift, zAtoms = row
+            if ".npy" not in fileName:
+                raise Exception(fileName + " is not a valid filename")
+            image = np.load(os.path.join(imgPath, fileName))
+            if moments is None:
+                radius = 7#int(len(image)/2) - 1 
+                desc = ZernikeMoments(radius)
+            moments = desc.describe(image)          
+            np.save(os.path.join(f"measurements_{testOrTrain}", fileName), moments)
+            
+            #Writer.writerow([fileName] + [str(difParams) for difParams in [element, xAtomRel, xAtomShift, yAtomRel, yAtomShift]])    
+            Writer.writerow([fileName] + [str(difParams) for difParams in [element, xAtomRel, yAtomRel, zAtoms]])
 
-with open('measurements_train\\labels.csv', 'w+', newline='') as labelsZernike, open(imgPathTrain + '\\labels.csv', 'r', newline='') as labelsFullPixelGrid:
-    Writer = csv.writer(labelsZernike, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    Reader = csv.reader(labelsFullPixelGrid, delimiter=',', quotechar='|')
+# with open('measurements_train\\labels.csv', 'w+', newline='') as labelsZernike, open(imgPathTrain + '\\labels.csv', 'r', newline='') as labelsFullPixelGrid:
+#     Writer = csv.writer(labelsZernike, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+#     Reader = csv.reader(labelsFullPixelGrid, delimiter=',', quotechar='|')
     
-    for firstRow in Reader:
-        Writer.writerow(["fileName","element","xAtomRel","yAtomRel","zAtoms"])
-        break
+#     for firstRow in Reader:
+#         Writer.writerow(["fileName","element","xAtomRel","yAtomRel","zAtoms"])
+#         break
 
-    for row in tqdm(Reader, desc = "Converting Test Data"):
-        if "element" in row:
-            continue
-        fileName, element, xAtomRel, xAtomShift, yAtomRel, yAtomShift, zAtoms = row
-        if ".npy" not in fileName:
-            raise Exception(fileName + " is not a valid filename")
-        image = np.load(os.path.join(imgPathTrain, fileName))
-        moments = desc.describe(image)                       
-        np.save("measurements_train\\" + fileName, moments)
-        #Writer.writerow([fileName] + [str(difParams) for difParams in [element, xAtomRel, xAtomShift, yAtomRel, yAtomShift]])   
-        Writer.writerow([fileName] + [str(difParams) for difParams in [element, xAtomRel, yAtomRel, zAtoms]])
+#     for row in tqdm(Reader, desc = "Converting Test Data"):
+#         if "element" in row:
+#             continue
+#         fileName, element, xAtomRel, xAtomShift, yAtomRel, yAtomShift, zAtoms = row
+#         if ".npy" not in fileName:
+#             raise Exception(fileName + " is not a valid filename")
+#         image = np.load(os.path.join(imgPathTrain, fileName))
+#         moments = desc.describe(image)                       
+#         np.save("measurements_train\\" + fileName, moments)
+#         #Writer.writerow([fileName] + [str(difParams) for difParams in [element, xAtomRel, xAtomShift, yAtomRel, yAtomShift]])   
+#         Writer.writerow([fileName] + [str(difParams) for difParams in [element, xAtomRel, yAtomRel, zAtoms]])
