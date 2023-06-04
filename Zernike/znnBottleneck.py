9cddbdd22dc22c2f61f87042866e275a0b10a38a
Karl Haertel <haertelk@pool4.physik.hu-berlin.de>
2023-06-04 17:51:59 +0200

    newBottleneckedZNN

---
+++ Zernike/znnBottleneck.py
@@ -0,0 +1,57 @@
+from torch.nn import Module
+from torch.nn import Conv2d
+from torch.nn import Linear
+from torch.nn import MaxPool2d
+from torch.nn import ReLU
+from torch.nn import Tanh
+from torch import flatten, cat
+
+class znn(Module):
+    def __init__(self, inFeatures, outFeatures):
+        # call the parent constructor
+        super(znn, self).__init__()
+        # first set fc -> ReLU -> Bottleneck -> ReLU
+        self.fc1 = Linear(in_features=inFeatures, out_features=2000)
+        self.relu = ReLU()
+        self.fc1B = Linear(in_features=2000, out_features=20)
+        # initialize second set, third set etc.
+        self.fc15 = Linear(in_features=20, out_features=4000)
+        self.fc15B = Linear(in_features=4000, out_features=40)
+
+        self.fc2 = Linear(in_features=40, out_features=8000)
+        self.fc2B = Linear(in_features=8000, out_features=80)
+
+        #init middle layers
+        self.fcmiddle = Linear(in_features=80, out_features=8000)
+        self.fcmiddleB = Linear(in_features=8000, out_features = 80)
+
+        # initialize last sets
+        self.fc3 = Linear(in_features=80, out_features=4000)
+        self.fc3B = Linear(in_features=4000, out_features=40)
+
+        self.fc4 = Linear(in_features=40, out_features=1000)
+        self.fc4B = Linear(in_features=1000, out_features=10)
+
+        self.fc5 = Linear(in_features=10, out_features=outFeatures)
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+
+        x = self.fc15(x)
+        x = self.relu(x)
+
+        x = self.fc2(x)
+        x = self.relu(x)
+
+        x = self.fcmiddle(x)
+        x = self.relu(x)
+
+        x = self.fc3(x)
+        x = self.relu(x)
+
+        x = self.fc4(x)
+        x = self.relu(x)
+
+        output = self.fc5(x)
+	
+        return output
