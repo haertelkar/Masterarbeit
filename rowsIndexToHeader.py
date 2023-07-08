eaaa1ecdf42b83238772830e69db5c745d85158f
Karl Haertel <haertelk@pool4.physik.hu-berlin.de>
2023-07-08 10:10:22 +0200

    dictionaries for header

---
+++ rowsIndexToHeader.py
@@ -0,0 +1,8 @@
+rowsIndexToHeader = {
+    0:"filename",
+    1:"element1",2:"element2",3:"element3",
+    4:"xAtomRel1",5:"xAtomRel2",6:"xAtomRel3",
+    7:"yAtomRel1",8:"yAtomRel2",9:"yAtomRel3"
+}
+
+headerToRowsIndex = {v: k for k, v in rowsIndexToHeader.items()}
