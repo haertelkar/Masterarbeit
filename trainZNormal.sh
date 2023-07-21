c70e185fa43e519b658a590632764371b67f37a3
Karl Haertel <haertelk@pool4.physik.hu-berlin.de>
2023-07-21 16:14:02 +0200

    slurm scripts

---
+++ trainZNormal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Submit this script with: sbatch thefilename
+
+#SBATCH --time=3:00:00   # walltime
+#SBATCH --ntasks=10   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH -J "trainZNormal"   # job name
+#SBATCH --mail-user=haertelk@physik.hu-berlin.de   # email address
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+python3 train.py -v AdamWDenseOnlyClosest -e 150 -i 4,7 -m ormal
