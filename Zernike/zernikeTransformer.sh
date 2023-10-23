af766f4fed7b076764662d6149519b27974a13fa
Karl Haertel <haertelk@pool4.physik.hu-berlin.de>
2023-10-23 12:51:31 +0200

    changed to single node

--- Zernike/zernikeTransformer.sh
+++ Zernike/zernikeTransformer.sh
@@ -3,7 +3,7 @@
 #Submit this script with: sbatch thefilename
 
 #SBATCH --time=24:00:00   # walltime
-#SBATCH --nodes=20   # number of nodes
+#SBATCH --nodes=1   # number of nodes
 #SBATCH -J "zernTransf"   # job name
 #SBATCH --mail-user=haertelk@physik.hu-berlin.de   # email address
 #SBATCH --mail-type=END
@@ -11,4 +11,4 @@
 
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
-mpirun python3 ZernikeTransformer.py
+python3 ZernikeTransformer.py
