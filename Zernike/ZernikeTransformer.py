3e175a22a43c4a5a8b4aeff54a57b02b8e5269b8
Karl Haertel <haertelk@pool4.physik.hu-berlin.de>
2023-11-15 14:51:42 +0100

    progress saving and better formatting

--- Zernike/ZernikeTransformer.py
+++ Zernike/ZernikeTransformer.py
@@ -1,4 +1,7 @@
+import gc
+import glob
 import re
+import resource
 import numpy as np
 import os
 import csv
@@ -32,13 +35,36 @@ def splitForEachRank(arr, size):
     arrs.append(arr)
     return arrs
 
+def findUnusedFileName(path, fileNameNoModifications:str) -> str:
+    fileNameModified = f"{fileNameNoModifications}"
+    while(os.path.exists(os.path.join(path,f"{fileNameModified}.hdf5"))):
+        print(fileNameModified, "taken")
+        fileNameModified = fileNameModified + "x"
+    print(fileNameModified, "chosen")
+    return f"{fileNameModified}"
+
+def readProgressAcrossAllRuns(path:str):
+    fileNamesDone = set()
+    for progressFile in glob.glob(os.path.join(path, "*.progress")):
+        with open(progressFile) as pF:
+            for fileName in pF:
+                fileNamesDone.add(fileName)
+
+    return fileNamesDone
+
+
 def zernikeTransformation(pathToZernikeFolder = os.getcwd(), radius = 15, noOfMoments = 40, leave = True):
     oldDir = os.getcwd() 
     os.chdir(pathToZernikeFolder)
     ZernikeObject = None
-
     for testOrTrain in ["train", "test"]:
-        with h5py.File(os.path.join(imagePath(testOrTrain), "training_data.hdf5"), 'r') as totalImages, h5py.File(os.path.join(f"measurements_{testOrTrain}", f"training_data_{rank}.hdf5"), 'w') as zernikeTotalImages:
+        zernikeFileName = findUnusedFileName(f"measurements_{testOrTrain}", f"training_data_{rank}")
+        zernikeHdf5FileName = zernikeFileName + ".hdf5"
+        zernikeProgressFileName = zernikeFileName + ".progress"
+        fileNamesDone = readProgressAcrossAllRuns(f"measurements_{testOrTrain}")
+        comm.barrier()
+
+        with h5py.File(os.path.join(f"measurements_{testOrTrain}", zernikeHdf5FileName), 'w') as zernikeTotalImages:
             imgPath = imagePath(testOrTrain)
             imageFileNames = set()
             with open(os.path.join(imgPath, 'labels.csv'), 'r', newline='') as labelsFullPixelGrid:
@@ -50,7 +76,8 @@ def zernikeTransformation(pathToZernikeFolder = os.getcwd(), radius = 15, noOfMo
                 fileNameWithCoords = firstEntry[0]
                 xCoord, yCoord, fileName = seperateFileNameAndCoords(fileNameWithCoords)
                 imageFileNames.add(fileName)
-                image = np.array(totalImages[fileName])[xCoord, yCoord]
+                with h5py.File(os.path.join(imagePath(testOrTrain), "training_data.hdf5"), 'r') as totalImages:
+                    image = np.array(totalImages[fileName])[xCoord, yCoord]
                 if ZernikeObject is None:
                     radius = radius or int(len(image)/2)
                     ZernikeObject = Zernike(radius, image.shape[-1], noOfMoments)
@@ -62,10 +89,19 @@ def zernikeTransformation(pathToZernikeFolder = os.getcwd(), radius = 15, noOfMo
 
             if rank == 0: shutil.copy(os.path.join(imgPath, "labels.csv"), os.path.join(f"measurements_{testOrTrain}", "labels.csv"))
             shutil.copy(os.path.join(imgPath, "labels.csv"), os.path.join(f"measurements_{testOrTrain}", "labels.csv"))
-            for cnt, fileName in enumerate(tqdm(imageFileNames, desc= f"Going through files in measurements_{testOrTrain}", total = len(imageFileNames), leave = leave)):
+            totalNumberOfFiles = len(imageFileNames)
+            imageFileNames = imageFileNames.difference(fileNamesDone)
+            for cnt, fileName in enumerate(tqdm(imageFileNames, desc= f"Going through files in measurements_{testOrTrain}", total = totalNumberOfFiles, initial=len(fileNamesDone), leave = leave, disable = (rank != 0))):
                 if cnt%worldsize == rank:
-                    images = np.array(totalImages[fileName])
+                    tqdm.write(f"rank: {rank}\nfileName: {fileName} \nram usage: {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss}")
+                    with h5py.File(os.path.join(imagePath(testOrTrain), "training_data.hdf5"), 'r') as totalImages:
+                        images = np.array(totalImages[fileName])
                     ZernikeObject.zernikeTransform(fileName, images, zernikeTotalImages)
+                    images = None
+                    gc.collect()
+                    with open(os.path.join(f"measurements_{testOrTrain}",zernikeProgressFileName), "a+") as progressFile:
+                        progressFile.write(f"{fileName}\n")
+
 
     os.chdir(oldDir)
 
