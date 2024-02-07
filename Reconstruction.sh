c7d1f9b49315163a30e157c1a0cf096aa2d1ad77
Karl Haertel <haertelk@pool4.physik.hu-berlin.de>
2024-02-07 16:20:22 +0100

    sh scripts for Reconstrunction.py and ROP

---
+++ Reconstruction.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# SLURM SUBMIT SCRIPT
+#SBATCH --nodes=1               
+#SBATCH --ntasks-per-node=1    
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
+#SBATCH -J "TestRec"   # job name
+
+# Debugging flags (optional)
+export NCCL_DEBUG=INFO
+export PYTHONFAULTHANDLER=1
+
+# On your cluster you might need this:
+# export NCCL_SOCKET_IFNAME=^docker0,lo
+
+# Run your training script
+python3 Reconstruction.py 
