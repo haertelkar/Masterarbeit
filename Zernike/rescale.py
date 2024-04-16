c47344a2b49138172b122a2ad70b748f025cfa20
Karl Haertel <haertelk@pool4.physik.hu-berlin.de>
2024-04-16 08:13:45 +0200

    commented out error checking

--- Zernike/rescale.py
+++ Zernike/rescale.py
@@ -2,7 +2,7 @@ import h5py
 import numpy as np
 from tqdm import tqdm
 
-#go through the hdf5 file and scale and shift the data. The vectors are all of the same length. Scale featurewise
+#go through the hdf5 file and scale the data. The vectors are all of the same length. Scale featurewise
 for testOrTrain in ["test", "train"]:
     maximumValues = None
     with h5py.File(f"measurements_{testOrTrain}/training_data.hdf5", "r") as f:
@@ -13,6 +13,7 @@ for testOrTrain in ["test", "train"]:
                 
             maximumValues = np.maximum(np.abs(maximumValues), np.abs(zernikeImages))
 
+
     assert(maximumValues is not None)
 
     with h5py.File(f"measurements_{testOrTrain}/training_data.hdf5", "r+") as f:
@@ -20,7 +21,7 @@ for testOrTrain in ["test", "train"]:
             zernikeImages = np.array(f[key][...]).astype(np.float32)
             f[key][...] = zernikeImages/maximumValues
 
-    with h5py.File(f"measurements_{testOrTrain}/training_data.hdf5", "r+") as f:
-        for key in tqdm(f.keys(), desc = "Testing every feature seperately for scaling."):
-            if(not(np.all(np.array(f[key][...]).astype(np.float32) <= 1.1) and np.all(-1.1 < np.array(f[key][...]).astype(np.float32)))):
-                raise Exception(f"Scaling unsuccesful: f[key] = {f[key][...]}")
+    #with h5py.File(f"measurements_{testOrTrain}/training_data.hdf5", "r+") as f:
+    #    for key in tqdm(f.keys(), desc = "Testing every feature seperately for scaling."):
+     #       if(not(np.all(np.array(f[key][...]).astype(np.float32) <= 1.1) and np.all(-1.1 < np.array(f[key][...]).astype(np.float32)))):
+     #           raise Exception(f"Scaling unsuccessful: f[key] = {f[key][...]}")
