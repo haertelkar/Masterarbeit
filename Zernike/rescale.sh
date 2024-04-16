8f1103f1a29a95881a3b5a73777de60d5d7fe4fb
Karl Haertel <haertelk@pool4.physik.hu-berlin.de>
2024-04-16 08:14:00 +0200

    created slurm file to run rescale

---
+++ Zernike/rescale.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Submit this script with: sbatch thefilename
+
+#SBATCH --time=30:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH -J "rescale"   # job name
+#SBATCH --mail-user=haertelk@physik.hu-berlin.de   # email address
+
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+python3 rescale.py
