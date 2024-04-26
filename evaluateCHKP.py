ccda45524aa57cfe6e7972d13e19c9988a92e5d9
Karl Haertel <haertelk@pool4.physik.hu-berlin.de>
2024-04-26 11:41:15 +0200

    works now for CNN and ZNN

--- evaluateCHKP.py
+++ evaluateCHKP.py
@@ -11,27 +11,34 @@ for n in range(numberOfOSAANSIMoments + 1):
         if (n-m)%2 != 0:
             continue
         resultVectorLength += 1
-modelName = "FullPixelGridML"
+
 DIMTILES = 12
-if modelName == "Zernike":
-    
+
+
+version = "ZernikeNormal_RelNewLowerBatchSize"
+
+if "Zernike" in version:
     pool = 3
     numChannels=resultVectorLength * DIMTILES**2
-    numLabels = (DIMTILES**2)//3**2
+    numLabels = 9
+    modelName = "ZernikeNormal"
 else:
     numChannels = DIMTILES**2
-    numLabels = 9
-
+    numLabels = (DIMTILES**2)//3**2
+    modelName = "FullPixelGridML"
+    
 print("load model")
 model = loadModel(modelName = modelName, numChannels=numChannels, numLabels = numLabels)
 
-model = lightnModelClass.load_from_checkpoint(checkpoint_path = os.path.join("checkpoints",f"FullPixelGridML_relCNN2024","epoch=20-step=138747.ckpt"), model = model)
+epochAndStep = "epoch=13-step=7168"
+checkpoint_path = os.path.join("checkpoints",f"{version}",f"{epochAndStep}.ckpt")
+model = lightnModelClass.load_from_checkpoint(checkpoint_path = checkpoint_path, model = model)
 lightnModel = lightnModelClass(model)
 
 lightnDataLoader = ptychographicDataLightning(modelName, classifier = False, indicesToPredict = None)
 lightnDataLoader.setup()
 
-trainer = pl.Trainer()
+trainer = pl.Trainer(accelerator='gpu')
 trainer.test(model, dataloaders=lightnDataLoader.test_dataloader())
 
-evaluater(lightnDataLoader.test_dataloader(), lightnDataLoader.test_dataset, model, indicesToPredict = None, modelName = modelName, version = "evaluation", classifier=False)
+evaluater(lightnDataLoader.test_dataloader(), lightnDataLoader.test_dataset, model, indicesToPredict = None, modelName = modelName, version = f"evaluation_{version}_{epochAndStep}", classifier=False)
