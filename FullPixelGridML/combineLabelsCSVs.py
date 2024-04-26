83e2880564d6b5601d60c156434865919d3910c5
Karl Haertel <haertelk@pool4.physik.hu-berlin.de>
2024-04-26 11:45:42 +0200

    implemented output of nonzero indices

--- FullPixelGridML/combineLabelsCSVs.py
+++ FullPixelGridML/combineLabelsCSVs.py
@@ -9,9 +9,10 @@ def combineLabelsAndCSV(workingDir):
     for testOrTrain in ["train", "test"]:
         filesToDelete = []
         labelsToConcat = []
+        indicesToConcat = []
         filesToIgnore = []
         df = pd.DataFrame()
-        noOfCSVs = 0
+        noOfLabelCSVs = 0
         if len(os.listdir(os.path.join(workingDir,f"measurements_{testOrTrain}"))) <= 3:
             continue
         with h5py.File(os.path.join(workingDir,f"measurements_{testOrTrain}",'temp_copy.h5'),mode='w') as h5fw:
@@ -19,7 +20,10 @@ def combineLabelsAndCSV(workingDir):
                 if (".csv" == file[-4:]) and ("labels" in file):
                     labelsToConcat.append(file)
                     if file != "labels.csv": filesToDelete.append(file)
-                    noOfCSVs +=1
+                    noOfLabelCSVs +=1
+                if (".csv" == file[-4:]) and ("Indices" in file):
+                    indicesToConcat.append(file)
+                    if file != "fractionOfNonZeroIndices.csv": filesToDelete.append(file)
                 if ".hdf5" == file[-5:]:
                     try:
                         if "training_data.hdf5" != file: filesToDelete.append(file)
@@ -39,9 +43,18 @@ def combineLabelsAndCSV(workingDir):
 
         
                 
-        print(f"Found {noOfCSVs} files. Combining into one file. {len(filesToIgnore)} .hdf5-files were corrupted and are deleted with their csv.-counterparts")        
+        print(f"Found {noOfLabelCSVs} files. Combining into one file. {len(filesToIgnore)} .hdf5-files were corrupted and are deleted with their csv.-counterparts")        
 
         cntCor = 0
+        if len(labelsToConcat) != 0:
+            for file in indicesToConcat:
+                id = "_".join(".".join(file.split(".")[:-1]).split("_")[1:])
+                if f"{id}.hdf5" in filesToIgnore:
+                    continue
+                data = pd.read_csv(os.path.join(f"measurements_{testOrTrain}",file))
+                df = pd.concat([df, data], axis = 0)
+            df.to_csv(os.path.join(f"measurements_{testOrTrain}","fractionOfNonZeroIndices.csv"), index= False)
+
         for file in labelsToConcat:
             id = "_".join(".".join(file.split(".")[:-1]).split("_")[1:])
             if f"{id}.hdf5" in filesToIgnore:
