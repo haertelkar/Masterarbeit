6159351b26c50934b1cea98e270478b570525bdb
Karl Haertel <haertelk@pool4.physik.hu-berlin.de>
2024-05-31 14:29:19 +0200

    discontinued

--- Zernike/rescale.py
+++ Zernike/rescale.py
@@ -2,6 +2,9 @@ import h5py
 import numpy as np
 from tqdm import tqdm
 
+print("[INFO] This script is not necessary.")
+exit()
+
 #go through the hdf5 file and scale the data. The vectors are all of the same length. Scale featurewise
 for testOrTrain in ["test", "train"]:
     maximumValues = None
@@ -20,6 +23,10 @@ for testOrTrain in ["test", "train"]:
         for key in tqdm(f.keys(), desc =f"Scaling every feature seperately in measurements_{testOrTrain}."):
             zernikeImages = np.array(f[key][...]).astype(np.float32)
             f[key][...] = zernikeImages/maximumValues
+
+    with open("scalingFactor.csv", "w") as f:
+        for scalar in maximumValues:
+            f.write(f"{scalar},")
 
     #with h5py.File(f"measurements_{testOrTrain}/training_data.hdf5", "r+") as f:
     #    for key in tqdm(f.keys(), desc = "Testing every feature seperately for scaling."):
