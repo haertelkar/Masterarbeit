0a83cb0bbafa9ca88fa5925d67c425726fb2ac35
Karl Haertel <haertelk@pool4.physik.hu-berlin.de>
2024-07-14 12:12:09 +0200

    adjusted for current output

--- evaluateCHKP.py
+++ evaluateCHKP.py
@@ -1,6 +1,7 @@
 from lightningTrain import loadModel, lightnModelClass, evaluater, ptychographicDataLightning
 import os
 import lightning.pytorch as pl
+import sys
 
 
 resultVectorLength = 0 
@@ -13,31 +14,36 @@ for n in range(numberOfOSAANSIMoments + 1):
         resultVectorLength += 1
 
 DIMTILES = 11
-DIMENSION = DIMTILES//2 + 1
+DIMENSION = DIMTILES//3 + 1
 
-version = "ZernikeNormal_1806_newDataDistFix_20n"
+version = "ZernikeNormal_0807_DistOverCubedDistOnlyDist"
 
 if "Zernike" in version:
     numChannels=resultVectorLength * DIMENSION**2
-    numLabels = 9
+    numLabels = 2
     modelName = "ZernikeNormal"
 else:
     numChannels = DIMENSION**2
-    numLabels = 9
+    numLabels = 2
     modelName = "FullPixelGridML"
     
 print("load model")
 model = loadModel(modelName = modelName, numChannels=numChannels, numLabels = numLabels)
 
-epochAndStep = "epoch=13-step=784"
+if len(sys.argv) > 1:
+    labelFile = sys.argv[1]
+else:
+    labelFile = "labels.csv"
+
+epochAndStep = "epoch=31-step=52480"
 checkpoint_path = os.path.join("checkpoints",f"{version}",f"{epochAndStep}.ckpt")
 model = lightnModelClass.load_from_checkpoint(checkpoint_path = checkpoint_path, model = model)
 lightnModel = lightnModelClass(model)
 
-lightnDataLoader = ptychographicDataLightning(modelName, classifier = False, indicesToPredict = None)
+lightnDataLoader = ptychographicDataLightning(modelName, classifier = False, indicesToPredict = None, labelFile=labelFile)
 lightnDataLoader.setup()
 
-trainer = pl.Trainer(accelerator='gpu')
+# trainer = pl.Trainer(accelerator='gpu')
 # trainer.test(model, dataloaders=lightnDataLoader.test_dataloader())
 
-evaluater(lightnDataLoader.test_dataloader(), lightnDataLoader.test_dataset, model, indicesToPredict = None, modelName = modelName, version = f"evaluation_{version}_{epochAndStep}", classifier=False)
+evaluater(lightnDataLoader.test_dataloader(), lightnDataLoader.test_dataset, model.to('cuda'), indicesToPredict = None, modelName = modelName, version = f"evaluation_{version}_{epochAndStep}", classifier=False)
