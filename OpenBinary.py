9c6968949b98b3355526cb9f41f23048f67584ea
Karl Haertel <haertelk@pool4.physik.hu-berlin.de>
2025-02-07 10:54:59 +0100

    goes through all ROP folders and creates images of the highest iteration

--- OpenBinary.py
+++ OpenBinary.py
@@ -8,7 +8,7 @@ import re
 
 def find_highest_numbered_file(directory, pattern=r"PotentialReal(\d+)\.bin"):
     highest_number = -1
-    highest_file = None
+    highest_file = "No File Found"
 
     # Iterate through the files in the directory
     for filename in os.listdir(directory):
@@ -19,29 +19,31 @@ def find_highest_numbered_file(directory, pattern=r"PotentialReal(\d+)\.bin"):
                 highest_number = number
                 highest_file = filename
 
-    return highest_file
+    return highest_file, highest_number
 
 
-
-###Specify file Dimension and path
-Dimension = 400
-path = find_highest_numbered_file(".")
-print("Reading file: ", path)
-###Open file
-data = open(path, 'rb').read()
-###Generate 2D array
-values = struct.unpack(Dimension * Dimension * 'f', data)
-values =  np.reshape(values,(Dimension,Dimension))
-###Plot array
-from matplotlib import pyplot as plt
-yrange=np.arange(Dimension)
-xrange=np.arange(Dimension)
-x , y=np.meshgrid(xrange,yrange)
-plt.contourf(x,y, values, 500, cmap='gray')
-###Specify some plot settings
-plt.title("Reconstruction at iteration 5", fontsize=20)
-plt.xticks(fontsize=16)
-plt.yticks(fontsize=16)
-cb = plt.colorbar()
-cb.ax.tick_params(labelsize = 16)
-plt.savefig("".join(path.split(".")[:-1]) + ".png" )
+for folder in ["PredROP","TotalPosROP", "TotalGridROP"] + [f"PredROP{i}" for i in range(2,10)]:
+    ###Specify file Dimension and path
+    Dimension = 500
+    path , iteration = find_highest_numbered_file(folder)
+    path = folder + "/" + path
+    print("Reading file: ", path)
+    ###Open file
+    data = open(path, 'rb').read()
+    ###Generate 2D array
+    values = struct.unpack(Dimension * Dimension * 'f', data)
+    values =  np.reshape(values,(Dimension,Dimension))
+    ###Plot array
+    from matplotlib import pyplot as plt
+    yrange=np.arange(Dimension)
+    xrange=np.arange(Dimension)
+    x , y=np.meshgrid(xrange,yrange)
+    plt.contourf(x,y, values, 500, cmap='gray')
+    ###Specify some plot settings
+    plt.title(f"Reconstruction at iteration {iteration}", fontsize=20)
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    cb = plt.colorbar()
+    cb.ax.tick_params(labelsize = 16)
+    plt.savefig("".join(path.split(".")[:-1]) + ".png" )
+    plt.close()
