7a44baf663c00c512a31ac22e32024ad96a8793f
Karl Haertel <haertelk@pool4.physik.hu-berlin.de>
2025-04-04 14:02:40 +0200

    rescale now goes through all hdf5 files

--- Zernike/rescale.py
+++ Zernike/rescale.py
@@ -1,30 +1,37 @@
 import h5py
 import numpy as np
 from tqdm import tqdm
-
-print("[INFO] This script is not necessary.")
-exit()
+import glob
+# print("[INFO] This script is not necessary.")
+# exit()
 
 #go through the hdf5 file and scale the data. The vectors are all of the same length. Scale featurewise
 for testOrTrain in ["test", "train"]:
     maximumValues = None
-    with h5py.File(f"measurements_{testOrTrain}/training_data.hdf5", "r") as f:
-        for key in tqdm(f.keys(), desc = f"Finding the maximum values to scale to one in measurements_{testOrTrain}."):
-            zernikeImages = np.array(f[key][...]).astype(np.float32)
-            if maximumValues is None:
-                maximumValues = np.absolute(np.copy(zernikeImages))
-                
-            maximumValues = np.maximum(np.abs(maximumValues), np.abs(zernikeImages))
+    for hdf5file in tqdm(glob.glob(f"measurements_{testOrTrain}/*.hdf5"), desc = f"Finding the maximum values to scale to one in measurements_{testOrTrain}."):
+        try:
+            with h5py.File(f"{hdf5file}", "r") as f:
+                for key in tqdm(list(f.keys()), desc = f"Finding the maximum values to scale to one in {hdf5file}.", leave=False):
+                    zernikeImages = np.array(f[key][...]).astype(np.float32)
+                    for zernikeImage in zernikeImages:
+                        if zernikeImage is None:
+                            raise Exception(f"zernikeImages is None: {hdf5file}{key}")
+                        if maximumValues is None:
+                            maximumValues = np.absolute(np.copy(zernikeImage))
+                            
+                        maximumValues = np.maximum(np.abs(maximumValues), np.abs(zernikeImage))
+        except BlockingIOError:
+            continue
 
 
     assert(maximumValues is not None)
 
-    with h5py.File(f"measurements_{testOrTrain}/training_data.hdf5", "r+") as f:
-        for key in tqdm(f.keys(), desc =f"Scaling every feature seperately in measurements_{testOrTrain}."):
-            zernikeImages = np.array(f[key][...]).astype(np.float32)
-            f[key][...] = zernikeImages/maximumValues
+    # with h5py.File(f"measurements_{testOrTrain}/training_data.hdf5", "r+") as f:
+    #     for key in tqdm(f.keys(), desc =f"Scaling every feature seperately in measurements_{testOrTrain}."):
+    #         zernikeImages = np.array(f[key][...]).astype(np.float32)
+    #         f[key][...] = zernikeImages/maximumValues
 
-    with open("scalingFactor.csv", "w") as f:
+    with open("scalingFactorsNew.csv", "w") as f:
         for scalar in maximumValues:
             f.write(f"{scalar},")
 
